4719cc73b4f62e5d03fa73197090189be9b0bb40
Nguyễn Hoàng Phúc Nguyên <nguyenhoangsang28091995@gmail.com>
2021-07-23 00:57:05 +0700

    add Toast n EditText ID + Password

--- app/src/main/java/com/example/firstandroidapp/MainActivity.java
+++ app/src/main/java/com/example/firstandroidapp/MainActivity.java
@@ -6,27 +6,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.button);
+        EditText textId = findViewById(R.id.edittext1);
+        EditText textPassword = findViewById(R.id.edittext2);
+        Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openMainActivity2();
+                if (textId.getText().toString().equals("") || textPassword.getText().toString().equals("")) {
+                    Toast.makeText(MainActivity.this, "You not yet input username or password. Please recheck!", Toast.LENGTH_LONG).show();
+                } else {
+                    openActivity2();
+                }
             }
         });
     }
-    public void openMainActivity2 () {
-        Intent intent = new Intent(this,MainActivity2.class);
+
+    public void openActivity2() {
+        Intent intent = new Intent(MainActivity.this, Activity2.class);
         startActivity(intent);
     }
-
 }
