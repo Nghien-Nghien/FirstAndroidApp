f61e234079418b5eac61473f852a7ba1dca53bae
Nguyễn Hoàng Phúc Nguyên <nguyenhoangsang28091995@gmail.com>
2021-07-25 03:00:59 +0700

    Add WebView on Activity2

--- app/src/main/java/com/example/firstandroidapp/Activity2.java
+++ app/src/main/java/com/example/firstandroidapp/Activity2.java
@@ -2,7 +2,11 @@ package com.example.firstandroidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.google.android.material.slider.Slider;
 import com.r0adkll.slidr.Slidr;
@@ -10,10 +14,27 @@ import com.r0adkll.slidr.model.SlidrInterface;
 
 public class Activity2 extends AppCompatActivity {
 
+    WebView webview;
+
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         SlidrInterface slide = Slidr.attach(this);
+        webview = (WebView) findViewById(R.id.webview);
+        webview.setWebViewClient(new WebViewClient());
+        webview.loadUrl("https://voz.vn");
+        WebSettings webSettings = webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webview.canGoBack()) {
+            webview.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
